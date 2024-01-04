a54820ffe1cb312972adc286afe79eb7e6e1afe0
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-04 14:28:00 -0500

    Import sequelize connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  console.log('Database synced');
+});
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
